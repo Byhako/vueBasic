e4cf28129d59974cdc96133fa33956c958b3da79
Ruben E Acosta <eldragondera1@gmail.com>
2019-08-07 21:04:13 -0500

    agregando vue-styled-components

---
+++ todo/src/components/Login/Login.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  Container
+    h1 Mi contenedor
+</template>
+
+<script>
+import { Container } from './styles';
+
+export default {
+  name: 'Login',
+  components: {
+    Container,
+  },
+};
+</script>
