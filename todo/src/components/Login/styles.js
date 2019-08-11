b5357d3e7b132947b0f34a92f57bc2743b929bd9
Ruben E Acosta <eldragondera1@gmail.com>
2019-08-10 21:08:29 -0500

    v-for and v-model

--- todo/src/components/Login/styles.js
+++ todo/src/components/Login/styles.js
@@ -1,4 +1,4 @@
-import styled from 'vue-styled-components'
+import styled from 'vue-styled-components';
 
 export const Container = styled.div`
   background-color: gray;
@@ -8,4 +8,6 @@ export const Container = styled.div`
   flex: 1;
   justify-content: center;
   align-items: center;
-`
+`;
+
+export const Box = styled.div``;
---
+++ todo/src/views/Basic.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+  div
+
+    h1 {{title}}
+
+    input(type='text' v-model="newFruit" v-on:keyup.enter='addFruit')
+    button(@click='addFruit') Agregar
+
+    ul
+      li(v-for='fruit of fruits') {{fruit.amount}} - {{fruit.name}}
+        span(v-if='fruit.amount === 0') - sin stock
+
+</template>
+
+
+<script>
+// @ is an alias to /src
+// import Login from '@/components/Login/Login.vue';
+
+export default {
+  name: 'basic',
+  // components: {
+  //   Login,
+  // },
+  data() {
+    return {
+      title: 'Mi hermoso título',
+      fruits: [
+        { name: 'Banana', amount: 5 },
+        { name: 'Melon', amount: 0 },
+        { name: 'Manzana', amount: 3 },
+        { name: 'Limon', amount: 2 },
+      ],
+      newFruit: '',
+    };
+  },
+
+  methods: {
+    addFruit() {
+      this.fruits.push({ name: this.newFruit, amount:0 });
+      this.newFruit = '';
+    },
+  },
+};
+</script>
